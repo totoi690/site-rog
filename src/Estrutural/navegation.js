f7b32f91f54e8f2a26acad0e9b93dcf694087299
totoi690 <tozaomello@gmail.com>
2021-02-25 11:47:00 -0300

    correções

--- src/Estrutural/navegation.js
+++ src/Estrutural/navegation.js
@@ -3,28 +3,28 @@ import React from 'react'
 class Navegation extends React.Component {
     render() {
         return (
-<nav class="navbar navbar-expand-lg navbar-dark bgd-secondary p-2 px-5 sticky-top">
-  <a class="navbar-brand" href="#">Rogério de Mello</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Início</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Carreira </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Diferenciais </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Escritório </a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bgd-secondary p-2 px-5 sticky-top">
+        <span class="navbar-brand clickable" onClick={() => {window.scrollTo({top: 0, behavior: 'smooth'})}}>Rogério de Mello</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <span class="nav-link clickable">Início</span>
+            </li>
+            <li class="nav-item">
+              <span class="nav-link clickable">Carreira </span>
+            </li>
+            <li class="nav-item">
+              <span class="nav-link clickable">Diferenciais </span>
+            </li>
+            <li class="nav-item">
+              <span class="nav-link clickable">Escritório </span>
+            </li>
+          </ul>
+        </div>
+      </nav>
         )
     }
 }
